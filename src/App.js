ee0e556f928c560c563bc2f7a1b3124de1813575
zxymax <497084653@qq.com>
2022-03-14 19:32:02 +0800

    异步获取数据及router-dom的使用

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {  BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-  {
-    id: 1,
-    text: 'id labore ex et quam laborum',
-    email: 'Eliseo@gardner.biz',
-    body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'quo vero reiciendis velit similique earum',
-    email: 'Jayne_Kuhic@sydney.com',
-    body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'odio adipisci rerum aut animi',
-    email: 'Nikita@garfield.biz',
-    body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
-    reminder: true,
-  },
-  ])
+  const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+
+    const getTasks = async () => {
+      const tasksFormServer = await fetchTasks()
+      console.log(tasksFormServer)
+      setTasks(tasksFormServer.tasks)
+    }
+
+    getTasks()
+  }, [])
+
+
+    const fetchTasks = async () => {
+      const res = await fetch('./db.json')
+      const data = await res.json()
+      return data
+
+    }
+  const addTask = async (task) => {
+    const res = await fetch('./db.json', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+    setTasks([...tasks, data])
+
+  }
+
+  /*
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task}
     setTasks([...tasks, newTask])
   }
+  */
+
 
   const deleteTask = (id) => {
     console.log(id)
@@ -47,13 +63,35 @@ function App() {
     } : task))
   }
 
-  return (
-    <div className='container'>
-      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+  /**
+   *
+   *
+      */
+
+  const Abc = () => {
+    return (
+      <>
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length>0 ? <Tasks tasks={tasks} onAdd={addTask} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Data To Show' }
+      </>
+    )
+  }
+
+  return (
+    <Router>
+    <div className='container'>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+
+
+    <Routes>
+      <Route path="/" element={<Abc />} />
+    <Route path='/about' element={<About />} />
+
+    </Routes>
+      <Footer />
 
     </div>
+    </Router>
   );
 }
 
